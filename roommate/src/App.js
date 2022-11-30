972eed7f0ba6f9ab9c10912a6c3bd80ed616e88b
Reima <reima.luoma@gmail.com>
2022-11-30 09:52:45 +0200

    Added FilterButton component, adjusted MovieCard component to have bottom margin

--- roommate/src/App.js
+++ roommate/src/App.js
@@ -1,24 +1,37 @@
 import Authentication from "./Components/Authentication";
 import MovieCard from "./Components/MovieCard";
+import FilterButton from "./Components/FilterButton";
 
 function App() {
   return (
     <>
       <div className="container">
         <div className="row">
-
           <div className="col-4 align-items-left logo">
             ROOMMATE
           </div>
-
           <Authentication />
+        </div>
+      </div>
 
+      <div className="container">
+        <div className="row">
+          <div className="col filter-columns">
+            <FilterButton filterName={"Genres"} />
+            <FilterButton filterName={"Duration"} />
+          </div>
         </div>
       </div>
 
       <div className="container">
         <div className="row">
           <div className="card-columns col text-end">
+            <MovieCard />
+            <MovieCard />
+            <MovieCard />
+            <MovieCard />
+            <MovieCard />
+            <MovieCard />
             <MovieCard />
             <MovieCard />
             <MovieCard />
--- roommate/src/Components/MovieCard.js
+++ roommate/src/Components/MovieCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const MovieCard = () => {
     return (
-        <div className='card shadow-sm' style={{width: "12rem"}}>
+        <div className='card shadow-sm mb-4' style={{width: "12rem"}}>
             <img src='https://via.placeholder.com/150/e2c34b' className='card-img-top img-fluid' alt='...'></img>
             <div className='card-body'>
                 <h5 className='card-title'>Movie title</h5>
