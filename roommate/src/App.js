6227eee5c133446348427f2a0f489536be7e858b
Reima <reima.luoma@gmail.com>
2022-12-02 13:41:02 +0200

    du

--- roommate/src/App.js
+++ roommate/src/App.js
@@ -9,7 +9,7 @@ function App() {
       <div className="container">
         <div className="row">
           <div className="col text-center logo">
-            ROOMMATE -dum
+            ROOMMATE -du
           </div>
           Authentication
         </div>
