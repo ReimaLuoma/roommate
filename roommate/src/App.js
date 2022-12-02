0322cd116aea076739d06598bfd5c5edb3257560
Reima <reima.luoma@gmail.com>
2022-12-02 13:59:37 +0200

    test new build settings in aws

--- roommate/src/App.js
+++ roommate/src/App.js
@@ -9,7 +9,7 @@ function App() {
       <div className="container">
         <div className="row">
           <div className="col text-center logo">
-            ROOMMATE -du
+            ROOMMATE
           </div>
           Authentication
         </div>
