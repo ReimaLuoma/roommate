dfd04f9ca7ebda340fe6d52dabe371c7f934d9ca
Reima <reima.luoma@gmail.com>
2022-12-15 14:52:23 +0200

    Removed PopularMovie component
    Modified Display and MovieCard components to use axios and TMDB api for content on landing page

--- roommate/src/Components/Display.js
+++ roommate/src/Components/Display.js
@@ -1,24 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import MovieCard from "./MovieCard";
+import env from 'react-dotenv';
 
 const Display = () => {
+
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        axios.get('https://api.themoviedb.org/3/movie/popular?api_key=' + env.REACT_APP_TMDB_API_KEY + '&language=en-US&page=1').then(response => {
+            setMovies(response.data.results);
+        }).catch(err => {
+            console.log(err);
+        })
+    }, [])
 
     return (
         <section>
             <div className="row mt-3">
-                <div className="col text-end card-columns">
-                    <MovieCard />
-                    <MovieCard />
-                    <MovieCard />
-                    <MovieCard />
-                    <MovieCard />
-                    <MovieCard />
-                    <MovieCard />
-                    <MovieCard />
-                    <MovieCard />
-                    <MovieCard />
-                    <MovieCard />
-                    <MovieCard />
+                <div className="col justify-content-evenly text-end card-columns">
+                    {
+                        movies.map((movie, index) => {
+                            return <MovieCard key={index} {...movie} />
+                        })
+                    }
                 </div>
             </div>
       </section>
--- roommate/src/Components/MovieCard.js
+++ roommate/src/Components/MovieCard.js
@@ -1,18 +1,28 @@
 import React, { useEffect } from 'react';
-import PopularMovie from '../TMDB API/PopularMovie';
+import axios from 'axios';
+import env from 'react-dotenv';
 
-const MovieCard = () => {
+const poster_URL = (posterpath) => {
+    return 'https://image.tmdb.org/t/p/w500' + posterpath;
+};
 
-    useEffect (() => {
-        <PopularMovie />
-    }, [])
+const GetRuntimeByMovieId = (id) => {
+    axios.get('https://api.themoviedb.org/3/movie/'+id+'?api_key=' + env.REACT_APP_TMDB_API_KEY + '&language=en-US').then(response => {
+        console.log(response.data.runtime, typeof response.data.runtime);
+        return response.data.runtime;
+    }).catch(err => {
+        console.log(err);
+    })
+};
+
+const MovieCard = ({title, poster_path, id}) => {
 
     return (
-        <div className='card box-shadow mb-5' style={{width: "12rem"}}>
-            <img src='https://via.placeholder.com/200/e2c34b' className='card-img-top img-fluid' alt='...'></img>
+        <div className='card box-shadow mb-5' style={{width: "12rem", height: "25rem"}}>
+            <img src={poster_URL(poster_path)} className='card-img-top img-fluid' alt='...'></img>
             <div className='card-body'>
-                <h5 className='card-title'>Movie title</h5>
-                <p className='card-title'>hh.mm</p>
+                <h5 className='card-title'><b>{title}</b></h5>
+                <p className='card-title'>{GetRuntimeByMovieId(id)} min</p>
             </div>
         </div>
     );
