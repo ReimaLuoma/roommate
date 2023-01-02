44bd63473d7bcf99f51c06d37c090903434d89d4
Reima <reima.luoma@gmail.com>
2023-01-02 10:20:26 +0200

    Displays different view for admin and default user
    Displays user's name in login view part of profile icon

--- roommate/src/Components/User.js
+++ roommate/src/Components/User.js
@@ -3,21 +3,44 @@ import Account from "./Account";
 import Loans from "./Loans";
 import Logo from "./Logo";
 import SignOutButton from "./SignOut";
+import Add from "./Add";
+import Remove from "./Remove";
 
 const User = ({user}) => {
-    return (
-        <section>
-            <div className="row mb-5">
-                <Logo />
 
-                <div className="col-6 d-flex justify-content-end align-items-center">
-                    <Loans />
-                    <Account userName={user.userName}/>
-                    <SignOutButton />
+    console.log(user.signInUserSession.idToken.payload['cognito:groups']);
+    let userGroup = user.signInUserSession.idToken.payload['cognito:groups'];
+
+    if(userGroup.includes('admin')){
+        return (
+            <section>
+                <div className="row mb-5">
+                    <Logo />
+    
+                    <div className="col-6 d-flex justify-content-end align-items-center">
+                        <Add />
+                        <Remove />
+                        <Account userName={user.attributes.name}/>
+                        <SignOutButton />
+                    </div>
                 </div>
-            </div>
-        </section>
-    )
+            </section>
+        )
+    }else {
+        return (
+            <section>
+                <div className="row mb-5">
+                    <Logo />
+
+                    <div className="col-6 d-flex justify-content-end align-items-center">
+                        <Loans />
+                        <Account userName={user.attributes.name}/>
+                        <SignOutButton />
+                    </div>
+                </div>
+            </section>
+        )
+    }
 }
 
 export default User;
