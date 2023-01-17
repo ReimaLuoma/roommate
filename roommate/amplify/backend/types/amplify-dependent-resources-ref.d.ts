a2aa65e9aa96e3b11ab9398575b21adc3a222020
Reima <reima.luoma@gmail.com>
2023-01-17 12:14:37 +0200

    changes to aws auth: removed unnecessary auth services

--- roommate/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ roommate/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -6,18 +6,6 @@ export type AmplifyDependentResourcesAttributes = {
       "RootUrl": "string"
     }
   },
-  "auth": {
-    "roommate": {
-      "AppClientID": "string",
-      "AppClientIDWeb": "string",
-      "CreatedSNSRole": "string",
-      "IdentityPoolId": "string",
-      "IdentityPoolName": "string",
-      "UserPoolArn": "string",
-      "UserPoolId": "string",
-      "UserPoolName": "string"
-    }
-  },
   "function": {
     "mylambda": {
       "Arn": "string",
